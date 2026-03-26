1cc2c86c00d9fabdcbf833dc38fd9171df041c2b
Nitish <nitishkumar44470@gmail.com>
2026-03-26 13:50:16 +0530

    Update solution for Lab 01: API Fetcher

--- lab-01-api-fetcher/solution.py
+++ lab-01-api-fetcher/solution.py
@@ -2,17 +2,13 @@ import requests
 
 def fetch_issues(owner: str, repo: str) -> list:
     """Fetch open issues from a GitHub repository."""
-    url = f"https://api.github.com/repos/{owner}/{repo}/issues?state=open"
-    # Note: requests is imported at module level for simplicity in labs
-    try:
-        response = requests.get(url, timeout=5)
-        if response.status_code == 200:
-            return response.json()
-    except Exception:
-        pass
-    return []
+    # TODO: Build URL: https://api.github.com/repos/{owner}/{repo}/issues?state=open
+    # TODO: Fetch using requests.get()
+    # TODO: Return response.json() if status is 200, else []
+    pass
 
 
 def filter_open(issues: list) -> list:
     """Filter a list of issues to only include those where state is 'open'."""
-    return [i for i in issues if i.get("state") == "open"]
+    # TODO: Iterate over issues, return only those where state is 'open'
+    pass
